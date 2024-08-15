c30e8dd2bc0ffab2d4777a8d98fcbb4e72544e31
Praditya <pradityaaldi.s@gmail.com>
2024-08-15 17:33:35 +0700

    menambahkan react router dom routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
